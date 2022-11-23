f84a0c884397ede912875605ead30d804898016c
Hilora Dewett <h.dewett@outlook.com>
2022-11-23 13:46:47 -0500

    wordSearch function

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,15 @@
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
+  //  console.log(horizontalJoin);
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
-}
-
-module.exports = wordSearch
+    const verticalJoin = letters[0].map((x, i) => letters.map(y => y[i]).join(''))
+  //  console.log(verticalJoin)
+    for (l of verticalJoin) {
+      if (l.includes(word)) return true
+  }
+    return false;
+  };
+  
+  module.exports = wordSearch
